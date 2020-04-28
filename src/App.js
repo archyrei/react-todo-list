89e1bbd75332aa3f7f721d24b21b5a4581e648e2
Naufal <45205400+redmafia@users.noreply.github.com>
2020-04-28 10:38:39 +0700

    migrasi html css ke react

--- src/App.js
+++ src/App.js
@@ -3,9 +3,31 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-btn dark">Add</button>
+          <h2 className="header-title">to-do list</h2>
+          <button className="header-btn another-blue">Clear</button>
+        </section>
+        <section className="add">
+          <form className="add-form">
+            <input type="text" className="add-input dark" />
+            <button className="add-btn">Add</button>
+          </form>
+        </section>
+        <section className="list-todo">
+          <div className="todo">
+            <span className="todo-text">Brewing Kopi</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Brewing kopi</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Brewing kopi</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
