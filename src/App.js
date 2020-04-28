5652a3b6dbb2aceba8b628283dcfa8fdf956f523
Naufal <45205400+redmafia@users.noreply.github.com>
2020-04-28 11:12:08 +0700

    make paper component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from "react";
 import "./styles.css";
 
+import Paper from "./components/paper";
+
 export default function App() {
   return (
-    <div className="container">
-      <div className="frame">
-        <section className="header">
-          <button className="header-btn dark">Add</button>
-          <h2 className="header-title">to-do list</h2>
-          <button className="header-btn another-blue">Clear</button>
-        </section>
-        <section className="add">
-          <form className="add-form">
-            <input type="text" className="add-input dark" />
-            <button className="add-btn">Add</button>
-          </form>
-        </section>
-        <section className="list-todo">
-          <div className="todo">
-            <span className="todo-text">Brewing Kopi</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Brewing kopi</span>
-          </div>
-          <div className="todo">
-            <span className="todo-text">Brewing kopi</span>
-          </div>
-        </section>
-      </div>
-    </div>
+    <Paper>
+      <section className="header">
+        <button className="header-btn dark">Add</button>
+        <h2 className="header-title">to-do list</h2>
+        <button className="header-btn another-blue">Clear</button>
+      </section>
+      <section className="add">
+        <form className="add-form">
+          <input type="text" className="add-input dark" />
+          <button className="add-btn">Add</button>
+        </form>
+      </section>
+      <section className="list-todo">
+        <div className="todo">
+          <span className="todo-text">Brewing Kopi</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Brewing kopi</span>
+        </div>
+        <div className="todo">
+          <span className="todo-text">Brewing kopi</span>
+        </div>
+      </section>
+    </Paper>
   );
 }
